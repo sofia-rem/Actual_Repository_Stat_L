07144a8d0576306215b3a2bc319d43ab16a278fc
sofia-rem <sofia.rem2004@gmail.com>
2026-09-10 16:40:37 +0200

    Files 1 - test

---
+++ Stat_Book_Testt/Ch2/L1_3.R
@@ -0,0 +1,17 @@
+
+# install package
+#install.packages("devtools")
+#library(devtools)
+#install_github("adamdarwichkth/CM2018rpackage")
+#library(CM2018rpackage)
+
+#library(NHANES)
+
+#nhanes_subset <- NHANES %>%
+#  filter(Age > 18 & Gender == "male")
+
+#n_height <- sum(complete.cases(nhanes_subset$Height))
+
+#hist(nhanes_subset$Height, xlab="Height uwu", ylab="Freq ii", main = paste("NHANES: Height (N=", n_height, ")"))
+
+#library(NHANES)
